e5d0b5c594e951c563f2a9753ad1f598702933a0
milka77 <55999046+milka77@users.noreply.github.com>
2020-12-14 19:20:51 +0000

    changed header bg image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
+// Display contact information
 $('#contact-button').click(function(e) {
     $('#contact-row').toggleClass('hide');
     $('#contact-row').toggleClass('show');
+})
+
+// Navbar active link
+$('#navbarNavDropdown li').click(function(){
+    $('#navbarNavDropdown li').removeClass('active');
+    $(this).addClass('active');
 })
