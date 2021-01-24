0a0134a74a226babcd4fd842d5cd8493f01f5439
milka77 <55999046+milka77@users.noreply.github.com>
2021-01-24 17:31:56 +0000

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
-// Display contact information
-$('#contact-button').click(function(e) {
-    $('#contact-row').toggleClass('hide');
-    $('#contact-row').toggleClass('show');
-})
 
 // Navbar active link
 $('#navbarNavDropdown li a').click(function(){
     $('#navbarNavDropdown li a').removeClass('selected');
     $(this).addClass('selected');
+})
+
+// For the new design 
+// Display contact information
+$('#v-pills-direct-contact-tab').click(function(e) {
+    $('#direct-contact').toggleClass('hide-contact');
+    $('#direct-contact').toggleClass('show-contact');
 })
