dab7e368a003ada21695e3196a883a878364c2bb
milka77 <55999046+milka77@users.noreply.github.com>
2021-01-30 16:54:21 +0000

    added random js image sample

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,37 @@ let year = new Date();
 let currentYear = year.getFullYear();
 
 $('#year').text(currentYear);
+
+//Random picture Our Books
+const ourBooksArray = [
+    "assets/images/comming-soon/at_the_zoo.jpg",
+    "assets/images/comming-soon/farm.jpg",
+    "assets/images/comming-soon/find_10_hidden_objects.jpg",
+    "assets/images/comming-soon/my_village.jpg",
+    "assets/images/comming-soon/numbers.jpg",
+    "assets/images/comming-soon/professions.jpg",
+    "assets/images/comming-soon/rhymes.jpg",
+    "assets/images/comming-soon/work_machines.jpg"
+];
+
+let randomBooks = ourBooksArray[Math.floor(Math.random()*ourBooksArray.length)];
+
+$('#random-our-books-link').attr("href", randomBooks);
+$('#random-our-books-img').attr("src", randomBooks);
+
+// Random pictire Coming Soon
+const comingSoonArray = [
+    "assets/images/comming-soon/at_the_zoo.jpg",
+    "assets/images/comming-soon/farm.jpg",
+    "assets/images/comming-soon/find_10_hidden_objects.jpg",
+    "assets/images/comming-soon/my_village.jpg",
+    "assets/images/comming-soon/numbers.jpg",
+    "assets/images/comming-soon/professions.jpg",
+    "assets/images/comming-soon/rhymes.jpg",
+    "assets/images/comming-soon/work_machines.jpg"
+];
+
+let randomComingSoon = comingSoonArray[Math.floor(Math.random()*comingSoonArray.length)];
+
+$('#random-coming-soon-link').attr("href", randomComingSoon);
+$('#random-coming-soon-img').attr("src", randomComingSoon);
