00b038a744182bffe8d2be30e5ebd03cfaf9bd9f
milka77 <55999046+milka77@users.noreply.github.com>
2021-01-30 16:14:49 +0000

    new design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 
 // Navbar active link
-$('#navbarNavDropdown li a').click(function(){
-    $('#navbarNavDropdown li a').removeClass('selected');
-    $(this).addClass('selected');
+$('#nav a').click(function(){
+    $('#nav a').removeClass('active');
+    $(this).addClass('active');
 })
 
 // For the new design 
@@ -11,3 +11,10 @@ $('#v-pills-direct-contact-tab').click(function(e) {
     $('#direct-contact').toggleClass('hide-contact');
     $('#direct-contact').toggleClass('show-contact');
 })
+
+
+//Curent Year For Footer
+let year = new Date();
+let currentYear = year.getFullYear();
+
+$('#year').text(currentYear);
---
+++ new.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="keywords" content="child, book, books, publishing, art empire, kids">
+  <!-- CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="assets/css/style1.css">
+  <!-- JS -->
+  <script src="https://kit.fontawesome.com/b6c120cd7f.js" crossorigin="anonymous"></script>
+  <title>ART Empire Publishing</title>
+</head>
+<body>
+
+  <!-- Header -->
+  <header>
+    <div class="header-container">
+      <div class="row align-items-center w-100">
+        <div class="col-3">
+          <!-- Brand logo -->
+          <div class="frame">
+            <img src="assets/images/art-logo-only-lb-.png" alt="">
+          </div>
+          <!-- End of Brand logo -->
+        </div>
+        <div class="col-9 h-100">
+          <div class="text-center">
+            <p class="brand-name">Art Empire Publishing</p><br>
+          </div>
+          <div>
+            <span class="text-left">email</span>
+            <span class="text-right">phone</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </header>
+  <!-- End of Header -->
+
+  <!-- Main Content-->
+  <main>
+    <div class="container-fluid mt-4">
+      
+
+      <div class="row  mb-3 mr-1">
+        <div class="col-3 left">
+
+          
+          <!-- Divider -->
+          <div class="divider my-4  text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+          
+          <!-- Navigation -->
+          <div>
+            <nav id="nav">           
+              <a class="nav-btn nav-link" href="#"><i class="far fa-circle menu-icon"></i>Home</a>
+              <a class="nav-btn nav-link" href="#about-us"><i class="far fa-circle menu-icon"></i>About Us</a>
+              <a class="nav-btn nav-link" href="#our-books"><i class="far fa-circle menu-icon"></i>Our Books</a>
+              <a class="nav-btn nav-link" href="#coming-soon"><i class="far fa-circle menu-icon"></i>Coming Soon</a>
+              <a class="nav-btn nav-link" href="#contact-us"><i class="far fa-circle menu-icon"></i>Contact Us</a>
+            </nav>
+          </div>
+          <!-- End of Navigation -->
+
+          <!-- Divider -->
+          <div class="divider mt-3  text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+
+
+          <div id="direct-contact" class="contact mt-2 text-center">
+            <h4 class="text-center">Direct Contact</h4>
+            <p><i class="fas fa-envelope"></i> artempirepublishing@gmail.com</p>
+            <p><i class="fas fa-phone"></i> 0123548784</p>
+          </div>
+          <!-- Divider -->
+          <div class="divider mt-4  text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+          
+        </div>
+
+        <!-- Right side Content -->
+        <div class="col-9 right">
+
+          <!-- About Us -->
+          <div class="container-row mt-5" id="about-us">
+            <h3 class="text-center mt-4">About Us</h3>
+            <!-- Divider -->
+            <div class="divider mt-3 mb-3 text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+            <p class="text-justify">
+              Our company places great emphasis on making products that meet changing market trends, customer (typically parent) expectations, and primarily the needs of children.
+              We strive to ensure that the content of our books represents both the modern and authentic fairy-tale world because the rising generations must evolve in time, but they also need to know about the wonderful storytelling of their predecessors. These stories gave the young adult an aid and guidance for the independent life ahead of them. It was a moral guide that can still serve as a benchmark for learning and maintaining the correct values.
+            </p>
+
+            <h4 class="text-center mt-5 mb-2">Childrens and Books</h4>
+            <!-- Divider -->
+            <div class="divider mt-3 mb-3 text-center"><img class="divider-img w-50" src="./assets/images/divider.jpg" alt="divider"></div>
+
+            <div class="row">
+              <div class="col-6 text-justify">
+                For the little ones, being unable to read yet, the same information reaches through the visual world, with an emphasis on expanding their knowledge primarily. Based on the capabilities of this age group, a significant part of our publications will be represented by the foam books that have already revolutionized the book market, especially the publications for the younger age group.
+                <i class="fas fa-child"></i>
+              </div>
+              <div class="col-6  text-justify">
+                One of the most important features of books made of EVA foam is that they are light, easy to turn, and, thanks to the cardboard sheet covered with foil, liquids spilt on can be easily wiped off. They don’t have sharp edges, they can’t be easily torn apart, so they don’t cause injury.
+              </div>
+            </div>
+
+            <h4 class="text-center mt-5 mb-2">About Our Books</h4>
+            <!-- Divider -->
+            <div class="divider mt-3 mb-3 text-center"><img class="divider-img w-50" src="./assets/images/divider.jpg" alt="divider"></div>
+
+            <p>
+              The goods offered by us have been subjected to international official safety checks. The materials used in the process like the paper, offset mixture inks and the used hotmelt adhesive are all subject of testing.
+            </p>
+              <span class="about-us-list-name">The product’s specification had passed the UK and EU standard expectations as follows:</span>
+              <ul class="about-us-list mt-2">
+                <li>Standard consumer safety specification for toy safety <i class="fas fa-check"></i></li>
+                <li>Physical and mechanical properties <i class="fas fa-check"></i></li>
+                <li>Flammability <i class="fas fa-check"></i></li>
+                <li>Heavy Element Content in Paint and Similar Surface-Coating Materials and Substrate Materials <i class="fas fa-check"></i></li>
+              </ul>
+	
+          </div>
+          <!-- End of About Us -->
+          
+          <!-- Our Books -->
+          <div class="row our-books" id="our-books">
+
+            <div class="container-row">
+              <h3 class="text-center mt-4">Our Books</h3>
+              <!-- Divider -->
+              <div class="divider mt-3 mb-2 text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+
+              <p class="text-center">For a short presentation simple click on the book cover.</p>
+
+              <!-- Row for our books images -->
+              <div class="row text-center mx-5 my-5">
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <img class="our-books-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="" height="auto" width="80%">
+                </div>
+                
+              </div>
+              <!-- End of Row for our books images -->
+              
+            </div>
+          </div>
+          <!-- End of Our Books -->
+
+          <!-- Coming Soon -->
+          <div class="row coming-soon" id="coming-soon">
+
+            <div class="container-row">
+              <h3 class="text-center mt-4">Coming Soon</h3>
+              <!-- Divider -->
+              <div class="divider mt-3 mb-2 text-center"><img class="divider-img" src="./assets/images/divider.jpg" alt="divider"></div>
+
+              <p class="text-center">For a more detailed picture click on the selected book cover.</p>
+
+              <!-- Row for Coming Soon images -->
+              <div class="row text-center mx-5 my-5">
+
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/at_the_zoo.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/at_the_zoo.jpg" alt="At The Zoo book cover" height="auto" width="80%">
+                  </a>
+                </div>
+
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/farm.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/farm.jpg" alt="Farm book cover" height="auto" width="80%">
+                  </a>
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/find_10_hidden_objects.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/find_10_hidden_objects.jpg" alt="Find 10 Hidden Objects book cover" height="auto" width="80%">
+                  </a>
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/my_village.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/my_village.jpg" alt="My Village book cover" height="auto" width="80%">
+                  </a>
+                </div>
+
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/numbers.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/numbers.jpg" alt="Numbers book cover" height="auto" width="80%">
+                  </a>
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/professions.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/professions.jpg" alt="Professions book cover" height="auto" width="80%">
+                  </a>
+                </div>
+
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/rhymes.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/rhymes.jpg" alt="Rhymes book cover" height="auto" width="80%">
+                  </a>
+                </div>
+                
+                <div class="col-sm-12 col-md-4">
+                  <a href="assets/images/comming-soon/work_machines.jpg" target="_blank">
+                    <img class="coming-soon-img" src="assets/images/comming-soon/work_machines.jpg" alt="Work Machines book cover" height="auto" width="80%">
+                  </a>
+                </div>
+
+              </div>
+              <!-- End of Row for Coming Soon images -->
+            </div>
+          </div>
+          <!-- End of Coming Soon -->
+
+        </div>
+        <!-- End of Right side Content -->
+      </div>
+
+      <!-- Footer-->
+      <div class="row footer">
+
+        <!-- 1st column of footer-->
+        <div class="col-sm-12 col-md-4 pt-3" id="contact-us">
+          <h5 class="text-uppercase text-center">Contact us</h5>
+
+          <!-- Contact us Form -->
+          <form class="mx-5 mb-2">
+            <div class="form-group">
+              <input class="form-control" type="text" name="name" id="name" placeholder="Enter Your Name">
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="text" name="email" id="email" placeholder="Enter Your Email Address">
+            </div>
+            <div class="form-group">
+              <input class="form-control" type="text" name="subject" id="subject" placeholder="Enter A Subject">
+            </div>
+            <div class="form-group">
+              <textarea class="form-control" name="message" id="message"  rows="5" placeholder="Enter your message here"></textarea>
+            </div>
+            <div class="button text-center">
+              <button class="btn btn-outline-light px-5" type="submit" >Send</button>
+            </div>
+          </form>
+          <!-- End of Contact Us Form-->
+          
+        </div>
+        <!-- End of 1st column of footer-->
+        
+        <!-- 2nd column of footer-->
+        <div class="col-sm-12 col-md-4 pt-3 connect h-100">
+          <h5 class="text-uppercase">Connect With Us</h5>
+          <ul class="connect-list pt-4">
+            <li>
+              <a href="https://facebook.com" target="_blank">
+                <i class="fab fa-facebook"></i> Facebook
+              </a>
+            </li>
+            
+            <li>
+              <a href="mailto:artempirepublishing@gmail.com">
+                <i class="fas fa-envelope"></i> artempirepublishing@gmail.com
+              </a>
+            </li>
+
+            <li>
+              <i class="fas fa-phone"></i> 0123548784
+            </li>
+          </ul>
+
+        </div>
+        <!-- End of 2nd column of footer-->
+        
+        <!-- 3rd column of footer-->
+        <div class="col-sm-12 col-md-4 pt-3 site-links ">
+          <h5 class="text-uppercase">Site links</h5>
+
+          <ul class="site-links-list pt-4">
+            <li>
+              <a href="#">Home</a>
+            </li>
+
+            <li>
+              <a href="#About Us">About Us</a>
+            </li>
+
+            <li>
+              <a href="#our-books">Our Books</a>
+            </li>
+
+            <li>
+              <a href="#coming-soon">Coming Soon</a>
+            </li>
+          </ul>
+
+        </div>
+        <!-- End of 3rd column of footer-->
+
+        <div class="col-12 text-center mb-1">
+          <small class="text-bottom">All rights reserved. Copyright &copy;<span id="year"></span> Art Empire Publishing Ltd.</small>
+        </div>
+      </div>
+      <!-- End of Footer-->
+    </div>
+    <!-- End of container fluid -->
+  </main>
+  <!-- End of Main Content-->
+
+  <!-- jQuery, Popper.js, and Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+  <script src="assets/js/script.js"></script>
+</body>
+</html>
