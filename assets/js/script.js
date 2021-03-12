bbb929e1b430bb4cb0b9fe49d6cfe08e0a33b4c5
milka77 <55999046+milka77@users.noreply.github.com>
2021-03-12 10:12:09 +0000

    Added medium screen size style

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,5 +60,23 @@ $('#btnHamburger').click(function(e) {
     $('.bars').toggleClass('bg-light-brown');
 })
 
+// Back to the top function
+let backToTopBtn = document.getElementById('backToTopBtn');
 
+// When the user scrolls down 20px from the top, show the button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+    
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the page
+function backToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, FireFox, IE and Opera
+}
